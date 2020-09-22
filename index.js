c14f0c9a4aae8d734a37b5e41ae12d64a1d5025f
cyw <18604469853@163.com>
2020-09-22 13:21:20 +0800

    feat: node js 接受命令行参数

---
+++ index.js
@@ -0,0 +1,17 @@
+let playAction = process.argv[process.argv.length - 1];
+
+let rand = Math.random() * 3;
+let computerAction;
+if (rand < 1) {
+    computerAction = '1';
+} else if (rand > 1) {
+    computerAction = '2';
+} else {
+    computerAction = '3';
+}
+
+if (computerAction == playAction) {
+    console.log("相同");
+} else {
+    console.log("不同");
+}
